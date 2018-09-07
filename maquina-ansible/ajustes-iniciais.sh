af9cc592d593ec3a23defa428e547138a653f229
Sidney Pio de Campos <sidneypio@gmail.com>
2018-09-07 17:49:30 -0300

    ajustes na configuracao

--- maquina-ansible/ajustes-iniciais.sh
+++ maquina-ansible/ajustes-iniciais.sh
@@ -7,17 +7,20 @@ echo "Copiando arquivos de configuracao"
 /bin/cp hosts /etc/hosts
 /bin/cp hostname /etc/hostname
 /bin/cp sshd_config /etc/ssh/sshd_config
-/bin/cp repositories /etc/apk
 
+echo "ajustando repositorios"
+/bin/cp repositories /etc/apk
 /sbin/apk update
 
 echo "instalando dhpcd"
 /sbin/apk add dhcp
 /bin/cp dhcpd.conf /etc/dhcp/dhcpd.conf
 rc-update add dhcpd
+ 
+echo "instalando ansible"
+/sbin/apk add ansible
 
 echo "ajustes no git"
-/sbin/apk add git
 /usr/bin/git config --global user.email "sidneypio@gmail.com"
 /usr/bin/git config --global user.name "Sidney Pio de Campos"
 
