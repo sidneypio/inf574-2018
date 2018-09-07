afa9fe1094770fe67f1ac4f1bb4148cf3159fcf1
Sidney Pio de Campos <sidneypio@gmail.com>
2018-09-07 10:38:44 -0300

    adicionando configuracao de sshd no script de ajustes

--- maquina-ansible/ajustes-iniciais.sh
+++ maquina-ansible/ajustes-iniciais.sh
@@ -2,8 +2,15 @@
 #
 # ajustes iniciais
 #
+echo "Copiando arquivos de configuracao"
 /bin/cp interfaces /etc/network/interfaces 
 /bin/cp hosts /etc/hosts
+/bin/cp sshd_config /etc/ssh/sshd_config
+
+echo "ajustes no git"
+/sbin/apk add git
 /usr/bin/git config --global user.email "sidneypio@gmail.com"
 /usr/bin/git config --global user.name "Sidney Pio de Campos"
+
+echo "criando chave rsa"
 /usr/bin/ssh-keygen -t rsa
